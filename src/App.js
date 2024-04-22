a26f6ef21b2d0b450e2ebeb0a277cb12041d7749
imf22 <92887508+imf22@users.noreply.github.com>
2024-04-22 19:51:45 -0400

    Initial Image Slider

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import Accordian from './components/accordian';
 import RandomColor from './components/random-color';
 import StarRating from './components/star-rating';
+import ImageSlider from './components/image-slider';
 
 function App() {
   return (
     <div className="App">
       {/* <Accordian /> */}
       {/* <RandomColor /> */}
-      <StarRating noOfStars={10}/>
+      {/* <StarRating noOfStars={10}/> */}
+      <ImageSlider url={'https://picsum.photos/v2/list'} limit={'10'} page={'1'}/>
     </div>
   );
 }
